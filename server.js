bd6d1b6439bd621717beb00f60ae50312d9ed753
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2018-07-31 18:02:48 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const app = express();
+
+//Let's write out the first route
+app.get('/', (req, res) => res.send('Hello'));
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = 5000;
+app.listen(port, ()=> console.log(`server is running on port ${port}`));
