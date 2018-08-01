b13e86c73cf78e73148d9c842730130b427255f3
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2018-08-01 10:24:42 -0700

    added mongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:`mongodb://lynnkuh:${encodeURIComponent('newday13')}@ds018168.mlab.com:18168/devconnectorlynn`
+}
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
+
+
+
+//Db config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDb
+mongoose.connect(db)
+.then(() => console.log('MongoDb connected.'))
+.catch(err => console.log(err));
 
 //Let's write out the first route
 app.get('/', (req, res) => res.send('Hello'));
