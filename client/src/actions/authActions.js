f2eacc36ba99cd65579d2ac6901d874fba236b58
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2018-08-27 22:10:54 -0700

    Added TextFieldGroup and login and logout behavior

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,4 @@
-import {GET_ERRORS} from './types';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
@@ -49,3 +49,12 @@ export const setCurrentUser = decoded => {
   };
 };
 
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  //Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenicated to false
+  dispatch(setCurrentUser({}));
+}
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
+
 
 class Login extends Component {
   constructor() {
@@ -17,23 +21,42 @@ class Login extends Component {
     this.setState({[e.target.name]: e.target.value});
   }
 
+  // Call Login but already authenicated so go to dashboard
+
+  componentDidMount() {
+    if (this.props.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+
+    if (nextProps.errors) {
+    this.setState({ errors: nextProps.errors});
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
     const currentUser = {
       email: this.state.email,
-      password: this.state.password,
+      password: this.state.password
     }
   
+    this.props.loginUser(currentUser);
 
-    axios
-      .post('/api/users/login', currentUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
-
-  }
+    }
 
   render() {
+
+    // deconstruct this.state.errors
+
+    const { errors } = this.state;
+
     return (
       <div className="login">
     <div className="container">
@@ -42,14 +65,22 @@ class Login extends Component {
           <h1 className="display-4 text-center">Log In</h1>
           <p className="lead text-center">Sign in to your DevConnector account</p>
           <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
-              value={this.state.email} onChange={this.onChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"
-              value={this.state.password}onChange={this.onChange.bind(this)} />
-            </div>
+            <TextFieldGroup
+              placeholder="Email Address"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.onChange}
+              error={errors.email}
+            />
+            <TextFieldGroup
+              placeholder="Password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.onChange}
+              error={errors.password}
+            />
             <input type="submit" className="btn btn-info btn-block mt-4" />
           </form>
         </div>
@@ -60,4 +91,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
